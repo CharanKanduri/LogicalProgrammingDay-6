0148d3091710e536befea659a6f04cc13dd078aa
charan_kanduri <charan2000kanduri@gmail.com>
2021-07-02 17:21:55 +0530

    [CHARAN] Perfect Number added

--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -5,10 +5,54 @@ namespace LogicalProgramming
     class Program
     {
         static void Main(string[] args)
-        {   
+        {
+            Console.WriteLine("Select the required option\n1.Fibonacci Series\n2.Perfect Number\n3.Prime Number\n4.Reverse a number\n5.Coupon Numbers\n6.Simulate Stopwatch Program\n7.Find the Fewest Notes to be returned for Vending Machine\n8.dayOfWeek\n9.temperaturConversion\n10.monthlyPayment\n11.sqrt\n12.toBinary");
+            int inputNumber =Convert.ToInt32( Console.ReadLine());
 
+            switch (inputNumber)
+            {
+                case 1:
+                    FibonociSeries.ReadInput();
+                    break;
+                case 2:
+                    PerfectNumber.ReadInput();
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
 
-            FibonociSeries.ReadInput();
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+
+                    break;
+                case 11:
+
+                    break;
+                case 12:
+
+                    break;
+
+                default:
+                    Console.WriteLine("Select Correct Option");
+                    break;
+            }
+           
 
         }
     }
